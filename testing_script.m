f57791c1cf2edf4925a83c2c13a01b215806fac7
gaborod16 <gaborod16@gmail.com>
2017-04-20 13:33:33 +0100

    Flags for plotting classifiers

--- testing_script.m
+++ testing_script.m
@@ -1,74 +1,74 @@
 %% Binary classification KW + FisherLD
 load read_source.mat;
 kwb = FeatureProcess.KruskalWallis(data,meta,3,1);
-Classifier.FisherLD_bin(kwb);
+Classifier.FisherLD_bin(kwb,1);
 
 %% Binary classification KW + MinDistEuc
 load read_source.mat;
 kwb = FeatureProcess.KruskalWallis(data,meta,3,1);
-Classifier.MinDistEuc(kwb);
+Classifier.MinDistEuc(kwb,1);
 
 %% Binary classification KW + MinDistMah
 load read_source.mat;
 kwb = FeatureProcess.KruskalWallis(data,meta,3,1);
-Classifier.MinDistMah(kwb);
+Classifier.MinDistMah(kwb,1);
 
 %% Binary classification PCA + FisherLD
 load read_source.mat;
 pcab = FeatureProcess.PCA(data,3,1);
-Classifier.FisherLD_bin(pcab);
+Classifier.FisherLD_bin(pcab,1);
 
 %% Binary classification PCA + MinDistEuc
 load read_source.mat;
 pcab = FeatureProcess.PCA(data,3,1);
-Classifier.MinDistEuc(pcab);
+Classifier.MinDistEuc(pcab,1);
 
 %% Binary classification PCA + MinDistMah
 load read_source.mat;
 pcab = FeatureProcess.PCA(data,3,1);
-Classifier.MinDistMah(pcab);
+Classifier.MinDistMah(pcab,1);
 
 %% Binary classification LDA + FisherLD
 load read_source.mat;
 ldab = FeatureProcess.LDA(data,3,1);
-Classifier.FisherLD_bin(ldab);
+Classifier.FisherLD_bin(ldab,1);
 
 %% Binary classification LDA + MinDistEuc
 load read_source.mat;
 ldab = FeatureProcess.LDA(data,3,1);
-Classifier.MinDistEuc(ldab);
+Classifier.MinDistEuc(ldab,1);
 
 %% Binary classification LDA + MinDistMah
 load read_source.mat;
 ldab = FeatureProcess.LDA(data,3,1);
-Classifier.MinDistMah(ldab);
+Classifier.MinDistMah(ldab,1);
 
 %% Multiclass classification Kruskal + MinDistEuc
 load read_source.mat;
 kwm = FeatureProcess.KruskalWallis(data,meta,3,0);
-Classifier.MinDistEuc(kwm);
+Classifier.MinDistEuc(kwm,1);
 
 %% Multiclass classification Kruskal + MinDistMah
 load read_source.mat;
 kwm = FeatureProcess.KruskalWallis(data,meta,3,0);
-Classifier.MinDistMah(kwm);
+Classifier.MinDistMah(kwm,1);
 
 %% Multiclass classification PCA + MinDistEuc
 load read_source.mat;
 pcam = FeatureProcess.PCA(data,3,0);
-Classifier.MinDistEuc(pcam);
+Classifier.MinDistEuc(pcam,1);
 
 %% Multiclass classification PCA + MinDistMah
 load read_source.mat;
 pcam = FeatureProcess.PCA(data,3,0);
-Classifier.MinDistMah(pcam);
+Classifier.MinDistMah(pcam,1);
 
 %% Multiclass classification LDA + MinDistEuc
 load read_source.mat;
 ldam = FeatureProcess.LDA(data,3,0);
-Classifier.MinDistEuc(ldam);
+Classifier.MinDistEuc(ldam,1);
 
 %% Multiclass classification LDA + MinDistMah
 load read_source.mat;
 ldam = FeatureProcess.LDA(data,3,0);
-Classifier.MinDistMah(ldam);
+Classifier.MinDistMah(ldam,1);
