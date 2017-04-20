d43eae356379b06b0bedca17ee124a700706f184
MariaBranco <maria_jose_branco@hotmail.com>
2017-04-20 15:51:17 +0100

    cross validation e matriz confusao

--- Util.m
+++ Util.m
@@ -15,6 +15,22 @@ classdef Util
             distance = sqrt((v1-v2)*(v1-v2)');
         end
         
+        function result = statistics(conf_matrix)
+       
+            
+              
+                sprintf('accuracy = %.4f',sum(diag(conf_matrix)) / sum(sum(conf_matrix)))
+                if (size(conf_matrix,1) == 2)
+                    sprintf('precision = %.4f', conf_matrix(1,1) / (conf_matrix(1,1) + conf_matrix(2,1)))
+                    sprintf('sensitivity/recall = %.4f', conf_matrix(1,1) / (conf_matrix(1,1) + conf_matrix(1,2)))
+                    sprintf('specificity = %.4f', conf_matrix(2,2) / (conf_matrix(2,2) + conf_matrix(2,1)))
+                end
+                
+                conf_matrix
+            
+            
+        end
+        
         function conf_matrix = confusion_matrix(y_pred, y_test, show_out)
             conf_matrix = confusionmat(y_test, y_pred);
             
--- crossvalidation_testing.m
+++ crossvalidation_testing.m
@@ -1,13 +1,13 @@
 %% Cross validation test
 
 
-dataX = kwb.X_train;
-datay = kwb.y_train;
+dataX = ldab.X_train;
+datay = ldab.y_train;
 n_folds = 10;
 n_data = numel(datay);
 v_error = zeros(n_folds,1);
 
-xval = kwb;
+xval = ldab;
 [itrn,itst] = crossval(n_data,n_folds);
 final_matrix=zeros(2);
 
@@ -16,8 +16,8 @@ for i=1:n_folds
     xval.y_test = datay(:,itst{1});
     xval.X_train = dataX(itrn{1},:);
     xval.y_train = datay(:,itrn{1});
-    [~,conf_matrix,v_error(i)] = Classifier.FisherLD_bin(xval,0);
+    [~,conf_matrix,v_error(i)] = Classifier.MinDistMah(xval,0);
     final_matrix=final_matrix+conf_matrix;
 end
 
-final_matrix
+Util.statistics(final_matrix);
